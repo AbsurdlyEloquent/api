49546cf9043449eb45d5464f460a05615209ef30
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-09 13:24:35 -0400

    configuring apidoc

--- server/router.js
+++ server/router.js
@@ -3,7 +3,10 @@ const controller = require('../db/controller/project')
 
 const router = new express.Router()
 
+/* @api {get} /user/id: */
 router.get('/', controller.index)
+/* @api {get} '/q?name=$NAME' or '/q?lang=$LANG'
+ * finds projects based on name or language */
 router.get('/q?', controller.byName)
 
 module.exports = router
